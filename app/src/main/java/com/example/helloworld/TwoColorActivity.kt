7bf9b5e90e582cfc305c12bd5360fb026018764e
Tchaikovsky1114 <forwarm5891@gmail.com>
2023-03-22 17:29:08 +0900

    fragment 교체방법 및 intentfilter로 초기화면 지정 방법

--- app/src/main/java/com/example/helloworld/TwoColorActivity.kt
+++ app/src/main/java/com/example/helloworld/TwoColorActivity.kt
@@ -2,10 +2,28 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class TwoColorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_two_color)
+        settingButtons()
+    }
+
+    fun settingButtons() {
+        val buttonRed = findViewById<Button>(R.id.button_red_fragment)
+        val buttonBlue = findViewById<Button>(R.id.button_blue_fragment)
+
+        buttonRed.setOnClickListener(){
+            val fragmentTransection = supportFragmentManager.beginTransaction()
+            fragmentTransection.replace(R.id.frame_layout,RedFragment())
+            fragmentTransection.commit()
+        }
+        buttonBlue.setOnClickListener(){
+            val fragmentTransection = supportFragmentManager.beginTransaction()
+            fragmentTransection.replace(R.id.frame_layout,BlueFragment())
+            fragmentTransection.commit()
+        }
     }
 }
